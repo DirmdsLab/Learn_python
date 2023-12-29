3008f9e3830b209bd13ab7f29c656bcef3478d7f
DirmdsLab <me.dirmdslab1204@gmail.com>
2023-12-29 21:01:25 +0700

    Next Day ^_^

---
+++ 01_Variables.py
@@ -0,0 +1,28 @@
+# Part 1 : variables 
+print("Part 1")
+
+x = 5
+y = "Turu"
+
+print(x)
+print(y)
+
+# cpp and java bilek :v 
+
+# Part 2 : casting
+print("\nPart 2")
+
+casting_int = int(5)
+casting_str = str(4)
+casting_float = float(3)
+
+print(casting_int)
+print(casting_str)
+print(casting_float)
+
+print()
+
+# Mengambil type 
+print(type(casting_int))
+print(type(casting_str))
+print(type(casting_float))
