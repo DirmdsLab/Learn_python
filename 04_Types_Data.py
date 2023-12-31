2a6796b1f45c35140b0fc97a1be1fff6ed5bdc90
DirmdsLab <me.dirmdslab1204@gmail.com>
2023-12-31 21:25:36 +0700

    Dah

---
+++ 04_Types_Data.py
@@ -0,0 +1,76 @@
+# str 
+x_str = "Hmph"
+print(x_str,"Dengan Type :",type(x_str))
+print()
+
+# int 
+x_int = 5
+print(x_int,"Dengan Type :",type(x_int))
+print()
+
+# float 
+x_float = 5.5
+print(x_float,"Dengan Type :",type(x_float))
+print()
+
+# complex 
+x_complex = 5j
+print(x_complex,"Dengan Type :",type(x_complex))
+print()
+
+# list 
+x_list = ["Hmph","Auhmm","Hmm"]
+print(x_list,"Dengan Type :",type(x_list))
+print()
+
+# tuple 
+x_tuple = ("Hmph","Auhmm","Hmm")
+print(x_tuple,"Dengan Type :",type(x_tuple))
+print()
+
+# range 
+x_range = range(5)
+print(x_range,"Dengan Type :",type(x_range))
+print()
+
+# dict 
+x_dict = {"Name" : "LordTuru","Age" : "999"}
+print(x_dict,"Dengan Type :",type(x_dict))
+print()
+
+# set 
+x_set = {"Mending","Turu","Ehehe"}
+print(x_set,"Dengan Type :",type(x_set))
+print()
+
+# frozenset 
+x_frozen = frozenset({"Mending","Turu","Ehehe"})
+print(x_frozen,"Dengan Type :",type(x_frozen))
+print()
+
+# bool 
+x_bool = True
+print(x_bool,"Dengan Type :",type(x_bool))
+print()
+
+# bytes 
+x_bytes = b"Turu"
+print(x_bytes,"Dengan Type :",type(x_bytes))
+print()
+
+# bytearray 
+x_bytearray = bytearray(5)
+print(x_bytearray,"Dengan Type :",type(x_bytearray))
+print()
+
+# memoryview 
+x_memoryview = memoryview(bytes(5))
+print(x_memoryview,"Dengan Type :",type(x_memoryview))
+print()
+
+# none 
+x = None
+print(x,"Dengan Type :",type(x))
+print()
+
+# udah kek buku cacatan :v 
---
+++ 05_Setting_Types_Data.py
@@ -0,0 +1,25 @@
+x = str("MendingTuru")
+print(x,"Type :",type(x))
+
+x = int(20)
+print(x,"Type :",type(x))
+
+x = float(5.2)
+print(x,"Type :",type(x))
+
+x = complex(10j)
+print(x,"Type :",type(x))
+
+x = list(("Turu","Mending","Turuuu"))
+print(x,"Type :",type(x))
+
+x = tuple(("Turu","Mending","Turuuu"))
+print(x,"Type :",type(x))
+
+x = set(("Turu","Mending","Turuuu"))
+print(x,"Type :",type(x))
+
+x = frozenset(("Turu","Mending","Turuuu"))
+print(x,"Type :",type(x))
+
+# part 2 dari type data 
