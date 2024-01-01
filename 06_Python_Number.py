ba0487c17c9773525a5b45d61e52e164ccc7f727
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-01 16:37:44 +0700

    Number

---
+++ 06_Python_Number.py
@@ -0,0 +1,44 @@
+# int 
+x = 1
+y = 1234567890
+z = -987654321
+
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print("Intinya bakal tetap int\n")
+
+# float 
+x = 1.5
+y = 14.12345
+z = -28.12345
+
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print("Intinya bakal tetap float\n")
+
+# complex 
+x = 10+5j
+y = 10j
+z = -10j
+
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print("Intinya bakal tetap complex\n")
+
+
+# convert type 
+x = 10
+print(x,type(x))
+
+x = float(x)
+print(x,type(x))
+
+x = complex(x)
+print(x,type(x))
+
+# random number 
+
+
