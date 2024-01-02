0cef471181d81ef35f8f09cff0b936d804e0b8c7
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-02 22:17:35 +0700

    update

---
+++ 08_Casting.py
@@ -0,0 +1,8 @@
+x = 5
+print(x,type(x))
+
+x = float(x)
+print(x,type(x))
+
+x = str(x)
+print(x,type(x))
---
+++ 09_String.py
@@ -0,0 +1,38 @@
+x = "Hmph"
+print(x)
+
+# another 
+print("Hmph")
+print()
+
+# multiline string 
+x = """Mending Turu Bang 
+Dan Lanjutkan Rebahan Mu
+Ehehe :v
+"""
+
+print(x)
+
+# string are array 
+x = "Hmph"
+print(x[0])
+print()
+
+# looping string 
+for x in "Mending Turu":
+    print(x)
+
+# string length 
+x = "Hmph"
+print(x,"Dengan Panjang :",len(x))
+
+# check string 
+Text = "Mending Turu"
+print("turu" in Text)
+
+if "Turu" in Text:
+    print("Yee Ada Turu")
+
+print ("Hmph" not in Text)
+if "Hmph" not in Text:
+    print("Tidak Ada Lo")
