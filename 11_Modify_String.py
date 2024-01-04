c04f1735ffd4c8d7c4c136444869cb999c5beb20
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-04 21:26:35 +0700

    Dah Lah

---
+++ 11_Modify_String.py
@@ -0,0 +1,24 @@
+Text = "Mending Turu"
+
+print(Text.upper())
+
+print(Text.lower())
+print()
+
+# remove white space 
+
+Text = "Mending Turu "
+print(Text,"Hmph")
+print(Text.strip(),"Hmph")
+print()
+
+# replace string 
+Text = "Mending Turu"
+print(Text.replace("M","e"))
+print(Text.replace("u","o"))
+print()
+
+# split 
+Text = "Mending#Turu"
+print(Text.split("#"))
+
