b93bb78397a2c57c82076aaeae0ec16e4f44a155
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-06 21:14:34 +0700

    Note

---
+++ 14_Escape_Characters.py
@@ -0,0 +1,18 @@
+Text = "Mending \"Turu\" Dan Tetap Rebahan"
+
+print(Text)
+
+Text = "Mending \"Turu\" \nTetap Rebahan"
+print(Text)
+
+
+# Note 
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
