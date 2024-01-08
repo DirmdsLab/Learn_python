2fc94b7ee01afe15936f755b7aa085f2d721c5fe
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-08 23:48:02 +0700

    :v

---
+++ 16_Boolean.py
@@ -0,0 +1,31 @@
+print(10 < 5)
+
+# if 
+
+if (10 < 5):
+    print("Trueee :v")
+
+if (5 < 10):
+    print("Falseee :v")
+
+
+# other 
+print(bool("MendingTuru"))
+
+x = 10
+print(bool(x))
+print()
+
+# bool empty 
+print(bool(""))
+
+
+x = True
+
+if (x == True):
+    print("Yee True :v")
+else:
+    print("Noooo :v")
+
+
+# simple bool 
