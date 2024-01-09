8d5de18caacdfcdbe26988347ce9e84a7fe97ccd
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-09 22:09:35 +0700

    Dikit

---
+++ 17_Python_Operator.py
@@ -0,0 +1,28 @@
+x = 10
+y = 5
+z = 3
+
+# tambah tambah 
+print(x,"+",y,"=",x + y)
+
+# kurang kurang :v 
+print(x,"-",y,"=",x - y)
+
+# kali kali ^_^ 
+print(x,"*",y,"=",x * y)
+
+# bagi bagi 
+print(x,"/",y,"=",x / y)
+
+# 2.0 :v 
+print(x,"//",y,"=",x // y)
+
+# modulus 
+print(x,"%",z,"=",x % z)
+
+# pangkat 
+print(x,"**",z,"=",x ** z)
+
+
+
+
